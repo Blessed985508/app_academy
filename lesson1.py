b2ffd2741daa03e8ddeb21542d1c81915b83179a
Blessed985508 <blessedmafagane@gmail.com>
2026-07-12 02:27:39 +0200

    initial commit

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+#making the computer speak 
+print("hello world!")
+
+#storing data inside a variable
+#helps in making sure data is stored in the container
+name = "phumi"
+print(name)
+
+
+#making our code interactive
+name = input("what is your name: ?")
+print(f"Greetings, {name}!")
