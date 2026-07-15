6c1530e960413f7c8263fe17dc4ba053a1cde23a
Blessed985508 <blessedmafagane@gmail.com>
2026-07-15 03:30:19 +0200

    Complete concert ticket booker project

---
+++ digital_ticket_counter.py
@@ -0,0 +1,12 @@
+print("\nThe Concert Ticket Booker\n")
+
+#Ask for user input
+name=input("Enter your name: ")
+band_name =input("Enter the name of the band/artist you want to see: ")
+
+#output
+print(f"\nHey, {name}! Your tickets to see {band_name} are booked successfully!")
+
+
+
+
