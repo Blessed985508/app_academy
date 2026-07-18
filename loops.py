d5dbb428bfa12b3e4d5527accb56d4cc20699ff6
Blessed985508 <blessedmafagane@gmail.com>
2026-07-18 21:26:13 +0200

    Created loops to help run the program

---
+++ loops.py
@@ -0,0 +1,11 @@
+#A countdown loop  
+count = 4 
+  
+while count > 0 :
+    print(f"Count is: {count}")
+    count = count -1
+
+#building a simple rap counter build 
+for rep in range(1,4 ): 
+    print(f"This is the rep number: {rep}")
+   
